d930c0b438bed507401d4f3e4e82001239361eb9
alex8396 <alex8396@naver.com>
2020-06-20 20:52:54 +0900

    initional commit

---
+++ app/src/main/java/com/jaesikgoon/mp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.jaesikgoon.mp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    public  void MyClick(View v){
+        Intent i = new Intent(this, MapsActivity.class);
+        startActivity(i);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/res/drawable/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+J
+ic_launcher_background.xml,d\c\dcd4441c1451b0d8da1c906171fa6e48acc8e180
